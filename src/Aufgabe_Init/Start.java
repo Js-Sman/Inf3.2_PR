1fc0efae909bc17481aad384c56a3ed1d98d6e26
Js_Sman <js-schuhmann@outlook.de>
2024-01-13 17:56:18 +0100

    INIT_Netbeans

---
+++ src/Aufgabe_Init/Start.java
@@ -0,0 +1,42 @@
+package Aufgabe_Init;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author JS
+ */
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
+/**
+ * Builder Class
+ * @author le
+ */
+public class Start
+{
+  public Start()
+  {
+
+  }
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) 
+  {
+      System.out.println("Init_Netbeans");
+    try
+    {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    }
+    catch (Exception ex)
+    {
+      JOptionPane.showMessageDialog(null, ex.toString());
+    }
+    new Start();
+  }
+}
